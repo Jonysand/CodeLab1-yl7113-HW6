1b8979c454fe6f043d7100b14b7ec6df884dc128
Yongkun Li <indusdesign@163.com>
2020-03-09 09:17:55 -0400

    updated

--- Week4-CodeLab1-HighScoreList/Assets/Scripts/MainThread.cs
+++ Week4-CodeLab1-HighScoreList/Assets/Scripts/MainThread.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using SimpleJSON;
 
 public class MainThread : MonoBehaviour
 {
@@ -27,6 +28,12 @@ public class MainThread : MonoBehaviour
     // For Data Saving
     private const string PLAY_PREF_KEY_HS = "High Score";
     private const string FILE_SCORE_RANK = "/score_rank.txt";
+    private bool isGameEnded = false;
+    public string playerName = "new player";
+    public GameObject inputPlayerName;
+    public GameObject playerNameText;
+    private const string fetchURL = "http://www.jonysandyk.com:8883/UnityDropGameData";
+    private const string updateURL = "http://www.jonysandyk.com:8883/UnityDropGameDataUpdate";
 
 
     private void Awake() {
@@ -42,12 +49,7 @@ public class MainThread : MonoBehaviour
     void Start()
     {   
         // Screen.SetResolution(1920, 1080, true, 60);
-        ScoreRankPanel.SetActive(false);
-        scoreText.GetComponent<Text>().text = "Score: "+score.ToString();
-        dropsCount = 0;
-        initTarget();
-        loadData(isInit: true);
-        targetScoreText.GetComponent<Text>().text = "Target Score: "+targetScore.ToString();
+        gameReset(0);
         StartButton.GetComponent<Button>().onClick.AddListener(StartButtonCliked);
     }
 
@@ -83,16 +85,6 @@ public class MainThread : MonoBehaviour
         targetScore = getTargetScore(currentLevel);
     }
 
-    // start/pause game
-    public void gamestart() {
-        GameStarted = !GameStarted;
-        if (GameStarted){
-            StartButton.transform.GetChild(0).GetComponent<Text>().text = "Pause";
-        }else{
-            StartButton.transform.GetChild(0).GetComponent<Text>().text = "Start";
-        }
-    }
-
     // Calculate target score for different levels
     private int getTargetScore(int level){
         float difFactorDeno = 5; // larger -> easier
@@ -112,43 +104,128 @@ public class MainThread : MonoBehaviour
     }
 
     // load rank data
-    private void loadData(bool isInit){
-        string hsString;
-        if (File.Exists(Application.dataPath+FILE_SCORE_RANK)){
-            hsString = File.ReadAllText(Application.dataPath + FILE_SCORE_RANK);
-        }else {
-            hsString = "0,0,0,0,0,0,0,0,0,";
-        }
-        string[] splitString = hsString.TrimEnd(',').Split(',');
-        int[] scoreRank = Array.ConvertAll(splitString, int.Parse);
+    private void loadData(bool isNewScore){
+        // fetch rank from server
+        List<int> scoreRank = new List<int>();
+        List<string> nameRank = new List<string>();
         string scoretext = "";
         bool highScoreInterted = false;
-        string allScoreString = "";
-        for (int i = 0; i < scoreRank.Length; i++){
-            if (score>scoreRank[i] && !highScoreInterted && !isInit){
-                for (int j = scoreRank.Length-1; j > i; j--){
-                    scoreRank[j] = scoreRank[j-1];
-                }
-                scoreRank[i] = score;
-                highScoreInterted = true;
-                scoretext += "* ";
+        playerName = playerNameText.GetComponent<Text>().text;
+        UnityWebRequest getRequest = new UnityWebRequest(fetchURL, "GET");
+        var rankListJson = JSON.Parse("{}");
+        getRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        getRequest.SendWebRequest();
+        if(getRequest.isNetworkError || getRequest.isHttpError) {
+            Debug.Log(getRequest.error);
+            // read local file - previous version
+            string hsString=""; // high score list as a string
+            if (File.Exists(Application.dataPath + FILE_SCORE_RANK)){
+                hsString = File.ReadAllText(Application.dataPath + FILE_SCORE_RANK);
+            }else {
+                hsString = "0,0,0,0,0,0,0,0,0,";
             }
-            scoretext += ("("+(i+1)+")"+". "+'\t'+scoreRank[i]+'\n');
-            allScoreString = allScoreString + scoreRank[i] + ",";
+            string[] splitString = hsString.TrimEnd(',').Split(',');
+            scoreRank = new List<int>(Array.ConvertAll(splitString, int.Parse));
+            scoretext = ""; // to render rank list
+            highScoreInterted = false; // check if new score
+            string allScoreString = ""; // to write score file
+            for (int i = 0; i < scoreRank.Count; i++){
+                // update rank list
+                if (score>scoreRank[i] && !highScoreInterted && isNewScore){
+                    for (int j = scoreRank.Count-1; j > i; j--){
+                        scoreRank[j] = scoreRank[j-1];
+                    }
+                    scoreRank[i] = score;
+                    highScoreInterted = true;
+                    scoretext += "* ";
+                }
+                scoretext += ("("+(i+1)+")"+". "+'\t'+scoreRank[i]+'\n');
+                allScoreString = allScoreString + scoreRank[i] + ",";
+            }
+            ScoreRankPanel.transform.GetChild(1).GetComponent<Text>().text = scoretext;
+            File.WriteAllText(Application.dataPath + FILE_SCORE_RANK, allScoreString);
         }
-        ScoreRankPanel.transform.GetChild(1).GetComponent<Text>().text = scoretext;
-        File.WriteAllText(Application.dataPath + FILE_SCORE_RANK, allScoreString);
+        else {
+            while(getRequest.downloadProgress < 1){}
+            rankListJson = JSON.Parse(getRequest.downloadHandler.text);
+            for (int i = 0; i < rankListJson.Count; i++){
+                nameRank.Add(rankListJson[i]["name"].Value);
+                scoreRank.Add(Int16.Parse(rankListJson[i]["score"].Value));
+            }
+            for (int i = 0; i < scoreRank.Count; i++){
+                // update rank list
+                if (score>scoreRank[i] && isNewScore && !highScoreInterted){
+                    scoreRank.Insert(i, score);
+                    scoreRank.RemoveAt(scoreRank.Count-1);
+                    highScoreInterted = true;
+                    scoretext += "* ";
+                    scoretext += ("("+(i+1)+")"+". "+'\t'+ playerName + '\t' + scoreRank[i]+'\n');
+                }else{
+                    scoretext += ("("+(i+1)+")"+". "+'\t'+ nameRank[i] + '\t' + scoreRank[i]+'\n');
+                }
+                
+            }
+            ScoreRankPanel.transform.GetChild(1).GetComponent<Text>().text = scoretext;
+        }
+
+        // upload new high score to server
+        if(isNewScore){
+            if (playerName == "") {playerName = "New Player";}
+            string json = JsonUtility.ToJson(new playerInfo(playerName, score));
+            Debug.Log(json);
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+            UnityWebRequest uwr = new UnityWebRequest(updateURL, "POST");
+            uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+            uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            uwr.SetRequestHeader("Content-Type", "application/json");
+            uwr.SendWebRequest();
+            if (uwr.isNetworkError || uwr.isHttpError){
+                Debug.Log(uwr.error);
+            }else{
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+
+    // start/pause game
+    public void gameStart() {
+        GameStarted = !GameStarted;
+        if (GameStarted){
+            StartButton.transform.GetChild(0).GetComponent<Text>().text = "Pause";
+            inputPlayerName.GetComponent<InputField>().interactable = false;
+        }else{
+            StartButton.transform.GetChild(0).GetComponent<Text>().text = "Start";
+            inputPlayerName.GetComponent<InputField>().interactable = true;
+        }
     }
 
     // Game Ends, read and saving data
     private void gameEnded(){
+        GameStarted = false;
+        isGameEnded = true;
         scoreText.GetComponent<Text>().text = "Game Over!, Your score is: "+score.ToString();
-        loadData(isInit: false);
+        loadData(isNewScore: true);
         ScoreRankPanel.SetActive(true);
     }
 
+    private void gameReset(int initDropCount){
+        isGameEnded = false;
+        score = 0;
+        currentLevel = 0;
+        targetScore = 0;
+        ScoreRankPanel.SetActive(false);
+        scoreText.GetComponent<Text>().text = "Score: "+score.ToString();
+        dropsCount = initDropCount;
+        initTarget();
+        loadData(isNewScore: false);
+        targetScoreText.GetComponent<Text>().text = "Target Score: "+targetScore.ToString();
+    }
+
     private void StartButtonCliked(){
-        gamestart();
+        if(isGameEnded){
+            gameReset(1);
+        }
+        gameStart();
         ScoreRankPanel.SetActive(GameStarted ? false:true);
     }
 }
