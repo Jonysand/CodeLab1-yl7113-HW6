812409f0c00c106968a149c6c50aee95f10160cd
Yongkun Li <indusdesign@163.com>
2020-03-21 18:38:50 -0400

    overlaping issue fixed

--- Week4-CodeLab1-HighScoreList/Assets/Scripts/playerInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿public class playerInfo{
-    public string playerName = "new player";
-    public int score = 0;
-
-    public playerInfo(string playerName, int score){
-        this.playerName = playerName;
-        this.score = score;
-    }
-}
