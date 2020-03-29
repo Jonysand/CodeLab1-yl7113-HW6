317914b7490510c8acacdd8c89c614d6ae91796f
Yongkun Li <indusdesign@163.com>
2020-03-29 16:13:03 -0400

    bug fixed

---
+++ Week6-CodeLab1-IntroToJSON/Assets/Scripts/playerInfo.cs
@@ -0,0 +1,9 @@
+﻿public class playerInfo{
+    public string playerName = "new player";
+    public int score = 0;
+
+    public playerInfo(string playerName, int score){
+        this.playerName = playerName;
+        this.score = score;
+    }
+}
